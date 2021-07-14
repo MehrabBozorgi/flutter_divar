6665a8d2970bdadf8b4a4d60b88018a3382be6e2
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-07-14 09:32:03 +0430

    flutter_divar

---
+++ lib/screen/add_ads/item/AddAds_AddImage2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'Add_Image_decor.dart';
+
+class AddAds_AddImage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Add_Image_decor(),
+        Add_Image_decor(),
+      ],
+    );
+  }
+}
