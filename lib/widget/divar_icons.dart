f8bf18ade46ce789dc0242037d604660b6a43b9f
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-07-15 11:18:43 +0430

    flutter_divar

---
+++ lib/widget/divar_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class Divar {
+  Divar._();
+
+  static const _kFontFam = 'Divar';
+
+  static const IconData divar = IconData(
+    0xe800,
+    fontFamily: _kFontFam,
+  );
+}
