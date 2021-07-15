7e60f7e65901f6281a44cce645e0aaca84f83470
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-07-15 11:19:20 +0430

    flutter_divar

---
+++ lib/widget/kConst.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const kTimeStyle =
+    TextStyle(fontSize: 14, fontFamily: 'normal', color: Colors.grey);
+
+const kTimeStyle2 = TextStyle(fontSize: 16, fontFamily: 'bold');
+const kTitleStyle = TextStyle(fontSize: 18, fontFamily: 'bold');
+
+const kInfoRow = TextStyle(fontSize: 14, fontFamily: 'normal');
+final kInfoRow4 =
+    TextStyle(fontSize: 14, fontFamily: 'normal', color: Colors.red[900]);
+const kInfoRow3 =
+    TextStyle(fontSize: 14, fontFamily: 'normal', color: Colors.grey);
+const kInfoRow2 = TextStyle(fontSize: 16, fontFamily: 'normal');
+const kButton =
+    TextStyle(fontSize: 14, fontFamily: 'normal', color: Colors.white);
+const kButton2 =
+    TextStyle(fontSize: 12, fontFamily: 'normal', color: Colors.white);
+
+const kInfoTozihat = TextStyle(fontSize: 12, fontFamily: 'normal');
+const kInfoTozihat3 = TextStyle(fontSize: 10, fontFamily: 'normal');
+const kInfoTozihat2 =
+    TextStyle(fontSize: 12, fontFamily: 'normal', color: Colors.grey);
+
+final kContainerStyle = BoxDecoration(
+  border: Border.all(
+    color: Color(0xFFD6D6D6),
+    width: 1,
+  ),
+  borderRadius: BorderRadius.circular(35),
+  color: Colors.transparent,
+);
+
+final kTextFormNumberStyle = OutlineInputBorder(
+  borderSide: BorderSide(color: Color(0xFFB71C1C), width: 1),
+  borderRadius: BorderRadius.circular(4),
+);
